011c7850517e79c8f14e7d15f214abeeddde0960
rcs <rolf.c.stadheim@gmail.com>
2016-01-03 21:30:59 +0100

    Added files

---
+++ src/main/groovy/rcstadheim/palenque/LeinDepsTask.groovy
@@ -0,0 +1,71 @@
+package rcstadheim.palenque 
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+
+class LeinDepsTask extends DefaultTask {
+
+    String projFileName
+
+    String gradleRepoPath = "/home/rcs/.gradle/caches/modules-2/files-2.1/"
+
+    @TaskAction
+    def curLeinDeps() {
+        def ld = getLeinDeps()
+        def splits = getSplits()
+        /*
+        ld.each { c ->
+            print c
+        }
+        splits.each { s ->
+            println s
+        }
+        */
+        //def newPfile = new File("/home/rcs/opt/java/vegaq/project.clj")
+
+        def newPfile = new File(projFileName)
+
+        newPfile.newWriter().withWriter { w ->
+            splits[0].eachLine { l ->
+                w << l << "\n"
+            }
+            w << "\t\t;deps\n"
+            ld.each { d ->
+                w << "\t\t" << d << "\n"
+            }
+            w << "\t\t;deps\n"
+            //splits[2].eachLine { l ->
+            splits.last().eachLine { l ->
+                w << l << "\n"
+            }
+        }
+    }
+
+    static def makeLibPath(curLib) {
+        "\t\"libs/${curLib}\""
+    }
+
+    def getSplits() {
+        //def pfile = new File("/home/rcs/opt/java/vegaq/project.clj").text
+        def pfile = new File(projFileName).text
+        pfile.split(";deps")
+    }
+
+    def getLeinDeps() {
+        def result = []
+        def allc = getProject().getConfigurations().getAsMap()
+        def compile = allc.get('compile')
+
+        compile.each {
+            def ar = it.canonicalPath.split(gradleRepoPath)
+            if (ar.length == 1) {
+                result << "\t\"${ar.getAt(0)}\""
+            } else {
+                result << makeLibPath(ar.getAt(1))
+            }
+        }
+
+        return result
+    }
+}
---
+++ src/main/groovy/rcstadheim/palenque/PalenquePlugin.groovy
@@ -0,0 +1,10 @@
+package rcstadheim.palenque 
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class PalenquePlugin implements Plugin<Project> {
+    void apply(Project target) {
+        target.task('leinDeps', type: LeinDepsTask)
+    }
+}
